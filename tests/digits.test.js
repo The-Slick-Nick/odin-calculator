184cbcef12bd318b8dd537aa48f3d6d2d417a617
The-Slick-Nick <96804604+The-Slick-Nick@users.noreply.github.com>
2024-05-18 15:58:29 -0500

    tests: Expand on tests for digit utilities

--- tests/digits.test.js
+++ tests/digits.test.js
@@ -3,7 +3,7 @@ const {
     numSignificantDecimalDigits
 } = require("../digits.js"); 
 
-/* LEFT OF DECIMAL */
+/* Nondecimal Digits */
 test("Single digit", () => {
     expect(numSignificantNondecimalDigits(1)).toEqual(1);
 });
@@ -27,4 +27,31 @@ test("Zero", () => {
 test("0 < n < 1", () => {
     expect(numSignificantNondecimalDigits(0.12)).toEqual(0);
 });
+
+
+/* Decimal Digits */
+test("Single decimal", () => {
+    expect(numSignificantDecimalDigits(1.1)).toEqual(1);
+});
+
+test("No decimal", () => {
+    expect(numSignificantDecimalDigits(14)).toEqual(0);
+});
+
+test("Multiple decimal", () => {
+    expect(numSignificantDecimalDigits(1.2345)).toEqual(4);
+});
+
+test("Negative decimal", () => {
+    expect(numSignificantDecimalDigits(-5.6789)).toEqual(4);
+});
+
+test("Zero nondecimal", () => {
+    expect(numSignificantDecimalDigits(0.14)).toEqual(2);
+});
+
+test("Trailing zeroes", () => {
+    expect(numSignificantDecimalDigits(1.234000)).toEqual(3);
+});
+
 
